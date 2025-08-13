f42501e704a66faaa4c8d298b0b4b01783628061
tusharikaJain <143836272+tusharikaJain@users.noreply.github.com>
2025-08-13 23:07:18 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
