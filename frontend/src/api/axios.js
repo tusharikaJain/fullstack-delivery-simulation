afda57e6bf19647e80fabd4392bae1ed458823c5
tusharikaJain <143836272+tusharikaJain@users.noreply.github.com>
2025-08-13 23:47:54 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://greencart-backend-gc4c.onrender.com'
 });
 
 API.interceptors.request.use(cfg => {
